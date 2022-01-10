9d24db8a94cd08736199292bb6709596fb3cdcd1
thaiminhpv <thaiminhpv@gmail.com>
2022-01-10 18:34:22 +0700

    add our own Turn server

--- www/script.js
+++ www/script.js
@@ -5,17 +5,9 @@ const ICE_SERVERS = [
 	{ urls: "stun:stun.sipnet.net:3478" },
 	{ urls: "stun:stun.ideasip.com:3478" },
 	{ urls: "stun:stun.iptel.org:3478" },
-	{ urls: "turn:numb.viagenie.ca", username: "imvasanthv@gmail.com", credential: "d0ntuseme" },
-	{
-		urls: [
-			"turn:173.194.72.127:19305?transport=udp",
-			"turn:[2404:6800:4008:C01::7F]:19305?transport=udp",
-			"turn:173.194.72.127:443?transport=tcp",
-			"turn:[2404:6800:4008:C01::7F]:443?transport=tcp",
-		],
-		username: "CKjCuLwFEgahxNRjuTAYzc/s6OMT",
-		credential: "u1SQDR/SQsPQIxXNWQT7czc/G4c=",
-	},
+        { urls: "turn:turn.thaiminhpv.xyz:5349", username: "thaiminhpv", credential: "1234" },
+        { urls: "turn:turn.thaiminhpv.xyz:3478", username: "thaiminhpv", credential: "1234" },
+        //{ urls: "turn:turn.thaiminhpv.xyz:3478", username: "thaiminhpv", credential: "1234" },
 ];
 
 const APP_URL = (() => {
